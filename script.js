d56540c2d4e7d4fbf8c4fc4ad4063891f32140ad
Alex <webmaster@papagai.de>
2022-02-09 14:19:49 +0100

    create

--- script.js
+++ script.js
@@ -2,34 +2,40 @@ const lists=document.querySelectorAll('.list')
 const elems = document.querySelectorAll('.elem')
 
 console.log(lists);
-console.log(elems)
+console.log(elems);
 
+const newElem = document.createElement('li')
+console.log(newElem);
 
-const  cloneElem1 = elems[3] // создаёт ссылку на елемент 3, а не новый елемент 3
-const cloneElem = elems[3].cloneNode() // создаёт пустой елемент  3 без содержымого внутри тега
-const cloneElem2 = elems[3].cloneNode(true) // через true получаем елемент с содержымым внутри тега
+newElem.textContent='step 6'
+newElem.classList.add('elem')
 
-//после клонирывания материнский елемент сохроняется
+//  ------   createElement    -----
+// создали новый елемент с классом 'elem'
 
-lists[1].append(cloneElem2)
+lists[1].append(newElem)
+console.log(newElem);
+//------------------------------------------
 
+const title=document.getElementById('title')
 
-console.log(cloneElem1)
-console.log(cloneElem)
-console.log(cloneElem2)
+title.insertAdjacentText('beforebegin','beforbegin') // добавил строку перед title 
+title.insertAdjacentText('afterend', 'afterend')// добавил строку после title 
 
-//------------------------------------------------------
+title.insertAdjacentText('beforeend', ' beforeend') // добавил строку в начале title 
+title.insertAdjacentText('afterbegin', 'afterbegin ')// добавил строку в конце title 
+//----------------------------------------------------
+title.insertAdjacentElement('beforebegin', elems[0]) // добавил елемент с классом перед title 
+title.insertAdjacentElement('afterend', elems[1])// добавил елемент с классом после title 
 
-cloneElem.classList.add('green-color')
-cloneElem.textContent = 'step 6'
-// создаём елемент в классе лист содержащим класс елем с продолжением класса green- color  = elem green-color , вставляя в него строку step 6
-console.dir(cloneElem)
+title.insertAdjacentElement('beforeend', elems[2]) // добавил елемент с классом в начале title 
+title.insertAdjacentElement('afterbegin', elems[3])// добавил елемент с классом в конце title 
+console.log(title);
 
+//-----------------------------------------------------
 
-cloneElem.innerHTML = '<strong>step 6</strong>'
-// задача строки через html , считывает теги
-lists[1].append(cloneElem)
-console.log(cloneElem)
+title.insertAdjacentHTML('beforebegin', '<span>step 1</span>') // добавил HTML строку перед title 
+title.insertAdjacentHTML('afterend','<span>step 1</span>')// добавил HTML строку после title 
 
-lists[0].innerHTML = ''
-//innerHTML переписывает или если ничего не задать стирает
+title.insertAdjacentHTML('beforeend','<span>step 1</span>') // добавил HTML строку в начале title 
+title.insertAdjacentHTML('afterbegin', '<span>step 1</span>')// добавил HTML строку в конце title
